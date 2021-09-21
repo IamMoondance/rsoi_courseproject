20a24c9e3be1c3e97ee19dce5ca16bfe977522e5
IamMoondance <daria-artiste@mail.ru>
2021-09-22 00:53:06 +0300

    pip3 ?..

--- Car/docker-entrypoint.sh
+++ Car/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 # Collect static files
 echo "Collect static files"
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:80
+python3 manage.py runserver 0.0.0.0:80
