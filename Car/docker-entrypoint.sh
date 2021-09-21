0a7487f52d6f68658378e324db0bed3967f7e231
IamMoondance <daria-artiste@mail.ru>
2021-09-22 00:31:27 +0300

    Исправлены ли порты в docker-контейнерах?

--- Car/docker-entrypoint.sh
+++ Car/docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 # Collect static files
 echo "Collect static files"
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 # Start server
 echo "Starting server"
-python3 manage.py runserver 0.0.0.0:80
+python manage.py runserver 0.0.0.0:80
