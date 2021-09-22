1ffd0bff94f4c439d1b163433fc07adcdfaebd70
IamMoondance <daria-artiste@mail.ru>
2021-09-22 16:24:15 +0300

    Исправлен docker-entrypoint

--- Car/docker-entrypoint.sh
+++ Car/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Collect static files
 echo "Collect static files"
-python3 manage.py collectstatic --noinput
+#python3 manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
